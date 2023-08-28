a9ee609882bc3b8c7944837bd11a650c422cd772
silvagomez <silva.camilo@gmail.com>
2023-08-28 23:42:11 +0200

    Upload C_00 ex03

---
+++ C_00/ex03/README.md
@@ -0,0 +1,14 @@
+### Exercice 03: [ft_print_alphabet](https://github.com/silvagomez/42piscine/blob/main/C_00/ex01/ft_print_numbers.c)
+
+| Rule              | Description         |
+| ----------------- | ------------------- |
+| Turn-in directory | ex03/               |
+| Files to turn in  | ft_print_numbers.c  |
+| Allowed functions | write               |
+
+Create a function that displays all digits, on a single line, by ascending order.
+<br><br>
+Here’s how it should be prototyped:
+```c
+void ft_print_numbers(void);
+```
---
+++ C_00/ex03/ft_print_numbers.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 19:24:48 by dsilva-g          #+#    #+#             */
+/*   Updated: 2023/08/28 20:50:38 by dsilva-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9' )
+	{
+		ft_putchar(number);
+		number++;
+	}
+}
